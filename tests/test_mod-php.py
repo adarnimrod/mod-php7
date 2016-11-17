0ed57caa6b0f70a38ca7d91ccbb1d440c6f31063
Adar Nimrod <nimrod@shore.co.il>
2016-11-17 23:00:41 +0200

    - Added phpinfo test. - Enable PHP Apache module.

--- tests/test_mod-php.py
+++ tests/test_mod-php.py
@@ -1,3 +1,7 @@
 def test_mod_php(Command, Sudo):
     with Sudo():
         assert 'php' in Command('a2query -m').stdout
+
+
+def test_phpinfo(Command):
+    'PHP Version 5' in Command('curl http://localhost/phpinfo.php').stdout
